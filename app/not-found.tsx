1266282628e7a1d3febf59cbdaa3aa7ecfbe39b6
Daniel Chung <daniel20020228@gmail.com>
2025-04-22 14:12:35 +1000

    learn how to use Metadata feature of Next.js

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,14 @@
 import Navigation from "./components/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Not found",
+};
 
 export default function NotFound() {
     return (
         <>
-            <Navigation /> <h1>Not Found</h1>
+            <h1>Not Found</h1>
         </>
     );
 }
